8bfc655e4c3ba5baf84818867f1180185969d4d5
Darryl <williams.de39@gmail.com>
2020-08-13 17:40:35 -0700

    heroku server

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MANGODB_URI = process.env.MANGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(MANGODB_URI)
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
